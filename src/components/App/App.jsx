bb6e45983a68c87f0e559a2be2a8c58b10120f1e
KSEN0X <130223300+KSEN0X@users.noreply.github.com>
2024-04-11 13:53:51 +0300

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,32 @@
+import css from './App.module.css';
+import { useEffect } from 'react';
+import ContactForm from '../ContactForm/ContactForm';
+import ContactList from '../ContactList/ContactList';
+import SearchBox from '../SearchBox/SearchBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from '../../redux/contactsOps.js';
+import { Toaster } from 'react-hot-toast';
+import Error from '../Error/Error.jsx';
+import Loader from '../Loader/Loader.jsx';
+
+export default function App() {
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.contacts.loading);
+  const error = useSelector(state => state.contacts.error);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div className={css.container}>
+      <Toaster />
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      {error && <Error errorMessage={`${error}`}> Error message: </Error>}
+      {loading && <Loader>Loading message</Loader>}
+      <ContactList />
+    </div>
+  );
+}
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,30 @@
+import css from './Contact.module.css';
+//npm install react-icons --save
+import { IoPersonSharp } from 'react-icons/io5';
+import { FaPhone } from 'react-icons/fa6';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contactsOps';
+
+export default function Contact({ id, name, number }) {
+  const dispatch = useDispatch(); 
+  const handleDelete = () => {
+    dispatch(deleteContact(id));
+  };
+  return (
+    <div className={css.contactCard}>
+      <ul className={css.contactList}>
+        <li className={css.contactItem}>
+          <IoPersonSharp />
+          {name}
+        </li>
+        <li className={css.contactItem}>
+          <FaPhone />
+          {number}
+        </li>
+      </ul>
+      <button className={css.btn} onClick={handleDelete}>
+        Delete
+      </button>
+    </div>
+  );
+}
